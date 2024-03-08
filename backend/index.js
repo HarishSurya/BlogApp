bd08cd1b2c23cdfdb144b92ae5c947c04fc0cb59
HarishSurya <harishtr30@gmail.com>
2024-03-09 00:33:23 +0530

    CORS enabled for all URLs

--- backend/index.js
+++ backend/index.js
@@ -38,10 +38,17 @@ app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
 //app.use(cors({origin:process.env.FRONTEND_URL,credentials:true}))
 //app.use(cors({origin:process.env.FRONTEND_URL,credentials:true}))
- app.use(cors({
-   origin: '*'
-   }));
+//  app.use(cors({
+//    origin: '*'
+//    }));
 //app.use(cors());
+app.use(cors({
+    origin: function(origin, callback){
+      return callback(null, true);
+    },
+    optionsSuccessStatus: 200,
+    credentials: true
+  }));
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
