a64f095c4c835b46a294765171aef0d533cb91d7
HarishSurya <harishtr30@gmail.com>
2024-03-08 23:59:56 +0530

    URL added

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,5 +1,7 @@
 // export const URL=import.meta.env.VITE_URL
 // export const IF=import.meta.env.VITE_IF
 // export const URL= "http://127.0.0.1:5173/"
-export const URL= "http://localhost:5000"
-export const IF="http://localhost:5000/images/"
+// export const URL= "http://localhost:5000"
+// export const IF="http://localhost:5000/images/"
+export const URL= "https://blogapp-1ftw.onrender.com/"
+export const IF="https://blogapp-1ftw.onrender.com/images/"
