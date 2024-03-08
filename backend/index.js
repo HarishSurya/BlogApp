8c358f19c7fe085a8c8304868454e48e3190da36
HarishSurya <harishtr30@gmail.com>
2024-03-08 23:03:39 +0530

    CORS added for images too

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,9 @@ const connectDB=async()=>{
 
 
 //middlewares
+app.use(cors({
+    origin: '*'
+  }));
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
@@ -42,12 +45,12 @@ app.use("/images",express.static(path.join(__dirname,"/images")))
 //     origin: '*'
 //   }));
 //app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
