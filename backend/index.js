c289bc097999ff8c51678eef95da8e4b79db6935
HarishSurya <harishtr30@gmail.com>
2024-03-08 23:51:58 +0530

    CORS enabled for FE URL

--- backend/index.js
+++ backend/index.js
@@ -32,28 +32,16 @@ const connectDB=async()=>{
 
 
 //middlewares
-// app.use(cors({
-//     origin: '*'
-//   }));
 dotenv.config()
 app.use(express.json())
-// app.use(cors({
-//     origin: '*'
-//   }));
-app.use("/images",express.static(path.join(__dirname,"/images")))
 //app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use("/images",express.static(path.join(__dirname,"/images")))
+app.use(cors({origin:process.env.FRONTEND_URL,credentials:true}))
 //app.use(cors({origin:process.env.FRONTEND_URL,credentials:true}))
-//app.use(cors({origin:process.env.FRONTEND_URL,credentials:true}))
-app.use(cors({
-    origin: '*'
-  }));
+//  app.use(cors({
+//    origin: '*'
+//    }));
 //app.use(cors());
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
