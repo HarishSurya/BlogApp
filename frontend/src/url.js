99b20729eb2232d68a8afe91ed9611f2618fcf45
HarishSurya <harishtr30@gmail.com>
2024-03-09 00:01:01 +0530

    URL added

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -3,5 +3,5 @@
 // export const URL= "http://127.0.0.1:5173/"
 // export const URL= "http://localhost:5000"
 // export const IF="http://localhost:5000/images/"
-export const URL= "https://blogapp-1ftw.onrender.com/"
+export const URL= "https://blogapp-1ftw.onrender.com"
 export const IF="https://blogapp-1ftw.onrender.com/images/"
