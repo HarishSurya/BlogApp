63f1b645842fbc7516ce89561a038cf77875eff6
HarishSurya <harishtr30@gmail.com>
2024-03-08 22:49:39 +0530

    CORS headers added

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,13 @@ app.use("/images",express.static(path.join(__dirname,"/images")))
 // app.use(cors({
 //     origin: '*'
 //   }));
-app.use(cors());
+//app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
